5d275db37da73f293234dd5d08c6b8a0eb2fcdb6
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2026-01-04 07:35:41 +0200

    feat: integrate home page sections into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { Header } from './components/layout/Header'
 import { Footer } from './components/layout/Footer'
+import Hero from './components/home/Hero'
+import FeaturedItems from './components/home/FeaturedItems'
+import CategoryGrid from './components/home/CategoryGrid'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +11,11 @@ function App() {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+        <FeaturedItems />
+        <CategoryGrid />
+      </main>
       <Footer />
     </>
   )
